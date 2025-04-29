1da32bcfa65b89317f3803cb02cb4a4925c9037a
Sammy-g254 <gsam65303@gmail.com>
2025-04-29 10:42:04 +0300

    Third commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -60,4 +63,7 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.8.9")
     //Compose dependency
     implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
+
+
 }
